8d47351c8bab9a2cdd91422884f0974c8f98d852
azimny86 <azimny86@gmail.com>
2023-03-07 23:06:44 +0000

    Add testinng covryge

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from .models import Item
 
+
 class TestModels(TestCase):
 
     def test_done_defult_to_flase(self):
         item = Item.objects.create(name="Test Todo Item")
         self.assertFalse(item.done)
+
+    def test_item_string(self):
+        item = Item.objects.create(name="Test Todo Item")
+        self.assertEqual(str(item), "Test Todo Item")
--- todo/test_views.py
+++ todo/test_views.py
@@ -38,3 +38,10 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+    def test_can_edit(self):
+        item = Item.objects.create(name="Test todo Item")
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Update Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Update Name')
