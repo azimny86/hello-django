166e278810f1ace0a93162ed5aae80cf9b24107c
azimny86 <azimny86@gmail.com>
2023-03-07 22:51:33 +0000

    Add testing for models

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
+class TestModels(TestCase):
 
-
+    def test_done_defult_to_flase(self):
+        item = Item.objects.create(name="Test Todo Item")
+        self.assertFalse(item.done)
